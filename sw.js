d7af46084e95cc84185e042eec94b89b013bc636
Manu-TY <cobo.adm@gmail.com>
2026-09-02 17:12:33 -0300

    Update sw.js
    v1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gnc-cerca-v1';
+const CACHE_NAME = 'gnc-cerca-v2'; // Incrementamos versión
 const ASSETS = [
   './',
   './index.html',
@@ -6,14 +6,32 @@ const ASSETS = [
   './manifest.json'
 ];
 
+// Instalación inmediata
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
+// Borrar archivos viejos de versiones anteriores
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Buscar primero la versión más nueva en internet (Network First)
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
